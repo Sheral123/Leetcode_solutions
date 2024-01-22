9e4f05fc063e431420697edbabc30cac75848a6a
Sheral Singla <90202819+Sheral123@users.noreply.github.com>
2024-01-22 16:45:32 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (5.08%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
   
-    int f(vector<int>& nums,vector<int>& dp, int ind){
+    /*int f(vector<int>& nums,vector<int>& dp, int ind){
         if(ind == 0) return nums[0];
         if(ind < 0) return 0;
         if(dp[ind] != -1) return dp[ind];
@@ -10,13 +10,12 @@ public:
         int left = f(nums,dp, ind -1);
         
         return dp[ind] = max(left, right);
-    }
+    }*/
 
     int rob(vector<int>& nums) {
         vector<int> dp1(nums.size()-1,-1);
         vector<int> dp2(nums.size()-1,-1);
-        vector<int> nums1;
-        vector<int> nums2;
+        vector<int> nums1, nums2;
         if(nums.size() == 1) return nums[0];
         for(int i=0; i< nums.size(); i++){
             if(i != nums.size()-1){
@@ -26,10 +25,26 @@ public:
                 nums2.push_back(nums[i]);
             }
         }
-        int ans1 = f(nums1,dp1,nums1.size()-1);
-        int ans2 = f(nums2,dp2,nums2.size()-1);
-        cout<<nums1.size();
-        cout<<nums2.size();
-        return max(ans1,ans2);
+     
+        dp1[0] = nums1[0];
+        for(int i=1 ; i<nums1.size();i++){
+            int right = nums1[i];
+            if(i>1){
+                right += dp1[i - 2];
+            }         
+            int left = dp1[i-1];
+            dp1[i]= max(left,right);
+        }
+
+        dp2[0] = nums2[0];
+        for(int i=1 ; i<nums2.size();i++){
+            int right = nums2[i];
+            if(i>1){
+                right += dp2[i - 2];
+            }
+            int left = dp2[i-1];
+            dp2[i]= max(left,right);
+        }
+        return max(dp1[nums1.size()-1],dp2[nums2.size()-1]);
     }
 };
