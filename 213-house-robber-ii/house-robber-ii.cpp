dfa20bfa8e6e46608987393330ed4edd4fa77e37
Sheral Singla <90202819+Sheral123@users.noreply.github.com>
2024-01-22 16:00:35 +0530

    Time: 4 ms (29.63%) | Memory: 9.6 MB (5.08%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+  
+    int f(vector<int>& nums,vector<int>& dp, int ind){
+        if(ind == 0) return nums[0];
+        if(ind < 0) return 0;
+        if(dp[ind] != -1) return dp[ind];
+
+        int right = nums[ind] + f(nums,dp, ind - 2);
+        int left = f(nums,dp, ind -1);
+        
+        return dp[ind] = max(left, right);
+    }
+
+    int rob(vector<int>& nums) {
+        vector<int> dp1(nums.size()-1,-1);
+        vector<int> dp2(nums.size()-1,-1);
+        vector<int> nums1;
+        vector<int> nums2;
+        if(nums.size() == 1) return nums[0];
+        for(int i=0; i< nums.size(); i++){
+            if(i != nums.size()-1){
+                nums1.push_back(nums[i]);      
+            }
+            if(i!=0){
+                nums2.push_back(nums[i]);
+            }
+        }
+        int ans1 = f(nums1,dp1,nums1.size()-1);
+        int ans2 = f(nums2,dp2,nums2.size()-1);
+        return max(ans1,ans2);
+    }
+};
