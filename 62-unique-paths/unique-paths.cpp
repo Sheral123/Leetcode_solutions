d3e8bf3226348e98aa3418c1f6ed47ac8449fe4c
Sheral Singla <90202819+Sheral123@users.noreply.github.com>
2024-02-05 16:33:19 +0530

    Time: 3 ms (33.19%) | Memory: 7.6 MB (14.75%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
  
     int func(int i, int j,vector<vector<int>> &dp){
@@ -17,3 +17,31 @@ public:
         return func(m,n,dp);
     }
 };
+*/
+class Solution {
+public:
+ 
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m+1, vector<int>(n+1,-1));
+        for(int i=1;i<=m;i++){
+            for(int j=1; j<=n;j++){
+                if(i==1 && j==1) dp[i][j]=1;
+                else{
+                    int up=0;
+                    int left =0;
+                    if(i>1){
+                        up = dp[i-1][j];
+                    }
+                    if(j>1){
+                        left = dp[i][j-1];
+                    } 
+
+                    dp[i][j] = up + left;               
+                }
+            }
+
+        }
+        return dp[m][n];
+    }
+};
+
