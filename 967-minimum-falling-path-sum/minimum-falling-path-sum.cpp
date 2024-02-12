a47c028c0d5f7d404bfd03e1d3fe2d7c0a858da5
Sheral Singla <90202819+Sheral123@users.noreply.github.com>
2024-02-12 19:21:41 +0530

    Time: 10 ms (74.65%) | Memory: 13.7 MB (5.69%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -1,7 +1,3 @@
-#include <vector>
-#include <climits>
-using namespace std;
-
 class Solution {
 public:
     int func(int i, int j, int n, vector<vector<int>>& matrix, vector<vector<int>>& dp){
@@ -26,36 +22,3 @@ public:
         return minSum;
     }
 };
-
-/*
-class Solution {
-public:
-
-    int helper(vector<vector<int>>& matrix, int i, int j, vector<vector<int>>& memo) {
-        int n = matrix.size();
-        if (i == n - 1) return matrix[i][j];
-
-        if (memo[i][j] != INT_MAX) return memo[i][j];
-
-        int dleft = (j > 0) ? helper(matrix, i + 1, j - 1, memo) : INT_MAX;
-        int ddown = helper(matrix, i + 1, j, memo);
-        int dright = (j < n - 1) ? helper(matrix, i + 1, j + 1, memo) : INT_MAX;
-
-        memo[i][j] = matrix[i][j] + min(ddown, min(dleft, dright));
-        return memo[i][j];
-    }
-
-    int minFallingPathSum(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        vector<vector<int>> memo(n, vector<int>(n, INT_MAX));
-        int minSum = INT_MAX;
-
-        for (int j = 0; j < n; ++j)
-            minSum = min(minSum, helper(matrix, 0, j, memo));
-
-        return minSum;
-    }
-
-
-};
-*/
