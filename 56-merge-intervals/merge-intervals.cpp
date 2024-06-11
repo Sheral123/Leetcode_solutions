01d7c44359dd55e2ad9989a6b388498a9fc654cb
Sheral Singla <90202819+Sheral123@users.noreply.github.com>
2024-06-11 23:42:58 +0530

    Time: 21 ms (73.68%) | Memory: 22.8 MB (30.68%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -9,14 +9,14 @@ public:
         for(int i=0 ; i<n ; i++){
             int start = intervals[i][0];
             int end = intervals[i][1];
-
-            if(!ans.empty() && end<=ans.back()[1]) continue;
-            for(int j = i+1; j<n; j++){
-                if(intervals[j][0] <= end){
-                    end= max(end, intervals[j][1]);
-                }
+            if(ans.empty() || start > ans.back()[1]){
+                ans.push_back({start,end});
             }
-            ans.push_back({start,end});
+
+            else{
+                ans.back()[1] = max(end,ans.back()[1] );
+            }
+
         }
         return ans;
     }
