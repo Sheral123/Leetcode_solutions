d884f4ae53202463064d759f9ce13207581d77de
Sheral Singla <90202819+Sheral123@users.noreply.github.com>
2024-08-06 17:18:20 +0530

    Time: 7 ms (35.90%) | Memory: 23.4 MB (16.03%) - LeetSync

---
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -0,0 +1,21 @@
+/*  TWO POINTER METHOD USING EXTRA SPACE */
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        int i=0, j, n=s.size();
+        string str;
+        while(i<n)
+        {
+            while(i<n && s[i]==' ') i++;
+            if(i==n) break;
+            j = i+1;
+            while(j<n && s[j]!=' ')j++;
+            string sub = s.substr(i, j-i);
+            if(str.size()==0)str = sub;
+            else str = sub + " " + str;
+            i = j+1;
+        }
+        return str;
+    }
+};
