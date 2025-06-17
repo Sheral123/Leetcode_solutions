cfdd51aacf1fc01eda2ff0bf72b3dd975393e788
Sheral Singla <sheralsingla18@gmail.com>
2025-06-17 17:43:39 +0530

    Time: 195 ms (82.77%) | Memory: 10.4 MB (87.00%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,35 +1,32 @@
 class Solution {
 public:
-    bool dfs(vector<vector<char>>& board, int i, int j, int idx, string& word) {
-        if (idx == word.length()) return true; // full match
-        if (i < 0 || j < 0 || i >= board.size() || j >= board[0].size()
-            || board[i][j] != word[idx]) return false;
+
+    bool dfs(vector<vector<char>>& board, int i, int j, int idx, string& word){
+        if(idx == word.length()) return true;
+        if(i <0 || j<0 || i >= board.size() || j >= board[0].size() || board[i][j] != word[idx]) return false;
 
         char temp = board[i][j];
-        board[i][j] = '#'; // mark as visited
+        board[i][j] = '#';
+        bool res = dfs(board,i+1,j,idx+1,word) ||
+                   dfs(board,i-1,j,idx+1,word) || 
+                   dfs(board,i,j+1,idx+1,word) ||
+                   dfs(board,i,j-1,idx+1,word); 
 
-        // 4 directions
-        bool found = dfs(board, i + 1, j, idx + 1, word) ||
-                     dfs(board, i - 1, j, idx + 1, word) ||
-                     dfs(board, i, j + 1, idx + 1, word) ||
-                     dfs(board, i, j - 1, idx + 1, word);
-
-        board[i][j] = temp; // backtrack
-        return found;
+        board[i][j]= temp;
+        return res;
     }
 
     bool exist(vector<vector<char>>& board, string word) {
-        int n = board.size(), m = board[0].size();
+        int n = board.size();
+        int m = board[0].size();
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (board[i][j] == word[0]) {
-                    if (dfs(board, i, j, 0, word))
-                        return true;
+        for(int i=0 ; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(board[i][j] == word[0]){
+                    if(dfs(board, i, j, 0, word)) return true;
                 }
             }
         }
-
         return false;
     }
 };
